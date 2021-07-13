318cf019e6b4ad24b02d2a8646ed3d81edb3ab53
Damian Lis <foxprogramming93@gmail.com>
2021-07-13 23:29:13 +0200

    Created the basic structure in App.js based on created components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import Container from 'components/Container';
+import Title from 'components/Title';
+import List from 'components/List';
+import ListItem from 'components/ListItem';
+import { data } from 'data/films.json';
+
 function App() {
-  return <div></div>;
+  const { films, planets } = data;
+
+  return (
+    <div style={{ padding: 20, paddingTop: 70 }}>
+      <Container>
+        <Title main='Star Wars' sub='RECRUITMENT TASK' />
+        <List>
+          {films.map((film) => (
+            <ListItem
+              key={film.id}
+              id={film.id}
+              title={film.title}
+              dataX={planets}
+            />
+          ))}
+        </List>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
