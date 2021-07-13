1e64ab17fbd6f820ad8517e6570d9107cb0080e4
Damian Lis <foxprogramming93@gmail.com>
2021-07-13 23:26:39 +0200

    Created List component

---
+++ src/components/List/list.component.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { List } from './list.styles';
+
+const ListComponent = ({ children }) => {
+  return <List>{children}</List>;
+};
+
+export default ListComponent;
---
+++ src/components/List/list.styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components/macro';
+
+export const List = styled.ul`
+  margin: 0 auto;
+  padding: 0 20px;
+`;
