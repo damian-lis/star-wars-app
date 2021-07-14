d758ce4e4de11b51a8d30da57074b65868d402b6
Damian Lis <foxprogramming93@gmail.com>
2021-07-14 16:47:51 +0200

    Added global theme

---
+++ src/data/theme.js
@@ -0,0 +1,10 @@
+export default {
+  colors: {
+    light: '#00687F',
+    dark: '#474747',
+  },
+
+  breakpoints: {
+    mobile: '750px',
+  },
+};
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import GlobalStyles from './GlobalStyles';
+import { ThemeProvider } from 'styled-components';
+import theme from 'data/theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
