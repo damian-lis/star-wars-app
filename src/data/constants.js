740323c03c1a83fe1bb4168cf71587f865824fd7
Damian Lis <foxprogramming93@gmail.com>
2021-07-14 16:49:01 +0200

    Added columns category in constants.js

---
+++ src/data/constants.js
@@ -0,0 +1,9 @@
+export const COLUMNS = [
+  { Header: 'Planet Name', accessor: 'name' },
+  { Header: 'Rotation period', accessor: 'rotationPeriod' },
+  { Header: 'Orbital period', accessor: 'orbitalPeriod' },
+  { Header: 'Diameter', accessor: 'diameter' },
+  { Header: 'Climate', accessor: 'climates' },
+  { Header: 'Surface water', accessor: 'surfaceWater' },
+  { Header: 'Population', accessor: 'population' },
+];
