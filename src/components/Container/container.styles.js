8f1d33264ea0e2b60217e0b40d1c04b6f102b541
Damian Lis <foxprogramming93@gmail.com>
2021-07-14 16:57:14 +0200

    Modified the Container component styles

--- src/components/Container/container.styles.js
+++ src/components/Container/container.styles.js
@@ -1,9 +1,6 @@
 import styled from 'styled-components/macro';
 
 export const Container = styled.div`
-  width: 100%;
-  max-width: 1000px;
-  margin: 0 auto;
-  padding: 20px;
-  background-color: gray;
+  padding: 15px;
+  padding-top: 70px;
 `;
